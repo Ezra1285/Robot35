adabf43d7e764837b45153a760fcc26e3df6cdf6
Ezra1285 <ezraskoog@yahoo.com>
2024-03-14 15:35:28 -0600

    Homework1 update

--- homework1.py
+++ homework1.py
@@ -11,3 +11,7 @@ if __name__ == "__main__":
     robot_instance.headDown()
     robot_instance.centerHead()
     time.sleep(3)
+    robot_instance.move_forward()
+    time.sleep(1)
+    robot_instance.fullyStop()
+    robot_instance.close()
