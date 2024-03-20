fdf231c0b7bea901ae52aeb19a4551d95c5f4591
Ezra1285 <ezraskoog@yahoo.com>
2024-03-20 12:20:01 -0600

    test 5

--- project_1.py
+++ project_1.py
@@ -8,7 +8,7 @@ def main():
     robot_instance = control_robot.robot()
     robot_instance.move_forward()
     time.sleep(2)
-    # robot_instance.fullyStop()
+    robot_instance.fullyStop()
     robot_instance.reverse()
     time.sleep(2)
     # robot_instance.fullyStop()
