2242256080671c2cbf297f35a12780cdef4c2ea9
Ezra1285 <ezraskoog@yahoo.com>
2024-03-25 00:37:33 -0600

    finished the body and face with key bindings

--- project_3.py
+++ project_3.py
@@ -15,9 +15,10 @@ def create_circle(x, y, r, canvas, line_width=2, fill_color="white"): #center co
     y1 = y + r
     return canvas.create_oval(x0, y0, x1, y1, width=line_width, fill=fill_color)
 
-class RobotFace():
+class RobotAnimations:
     def __init__(self, window):
         self.window = window
+        self.count = 500
         # Create the canvas that sits ontop the window
         self.canvas = tk.Canvas(self.window, bg='gray', height=480, width=800) # 700 by 850 
         create_circle(425, 250, 200, self.canvas, 4, 'light gray') # Head
@@ -28,7 +29,39 @@ class RobotFace():
         #  Mouth if we need
         # self.mouth = create_circle(425, 500, 80, self.canvas, 2, "red")
         # self.canvas.create_oval(425, 500, 80, 80, width=2, fill="red")
+        self.curr_animation = "eyes"
         self.canvas.pack()
+
+    # TODO: Delete other canvas if needed and create canvas that will dipslay the words thr robot recives
+    def createWordScreen():
+        pass
+
+    def createBody(self):
+        # Create the canvas that sits ontop the window
+        self.canvas = tk.Canvas(self.window, bg='gray', height=480, width=800)
+        # Create the initial body
+        self.leftLeg = self.canvas.create_line(325,425,400,300, width=4) # Left leg
+        self.rightLeg = self.canvas.create_line(475,425,400,300, width=4) # Right leg
+        self.back = self.canvas.create_line(400,300,400,150, width=4) # back
+        self.head = create_circle(400, 100, 50, self.canvas, 3, "gray")
+        self.leftEye = create_circle(385, 85, 10, self.canvas, 3, "black")
+        self.rightEye = create_circle(415, 85, 10, self.canvas, 3, "black")
+        self.leftArm = self.canvas.create_line(325,250,400,175, width=4) # Left arm
+        self.rightArm = self.canvas.create_line(475,250,400,175, width=4) # Right arm
+        self.curr_animation = "body"
+        self.canvas.pack()
+
+    def switchBodytoEyes(self):
+        self.removeCurrentCanvas()
+        self.curr_animation = "eyes"
+        self.canvas = tk.Canvas(self.window, bg='gray', height=480, width=800) # 700 by 850 
+        create_circle(425, 250, 200, self.canvas, 4, 'light gray') # Head
+        create_circle(350, 200, 75, self.canvas, 3)  # eye 1
+        self.pupil1 = create_circle(350, 200, 30, self.canvas, 3, "black")  # Pupil 1
+        create_circle(500, 200, 75, self.canvas, 3)  # eye 2
+        self.pupil2 = create_circle(500, 200, 30, self.canvas, 3, "black")  # Pupil 2
+        self.canvas.pack()
+
 
     def idleEyes(self):
         x0, y0, x1, y1 = get_circle_coords(350, 200, 30, self.canvas)
@@ -37,48 +70,36 @@ class RobotFace():
         self.canvas.coords(self.pupil2, x0, y0, x1, y1)
 
     def lookUp(self, button_event):
+        if self.curr_animation != "eyes":
+            self.switchBodytoEyes()
         self.canvas.coords(self.pupil1, 315, 125, 375, 200)
         self.canvas.coords(self.pupil2, 475, 125, 535, 200)
 
     def lookDown(self, button_event):
+        if self.curr_animation != "eyes":
+            self.switchBodytoEyes()
         self.canvas.coords(self.pupil1, 325, 200 , 375, 300)
         self.canvas.coords(self.pupil2, 475, 200, 525, 300)
 
     def lookleft(self, button_event):
+        if self.curr_animation != "eyes":
+            self.switchBodytoEyes()
         x0, y0, x1, y1 = get_circle_coords(300, 200, 30, self.canvas)
         self.canvas.coords(self.pupil1, x0, y0, x1, y1)
         x0, y0, x1, y1 = get_circle_coords(450, 200, 30, self.canvas)
         self.canvas.coords(self.pupil2, x0, y0, x1, y1)
 
     def lookRight(self, button_event):
-        print("Buttom:", button_event)
+        if self.curr_animation != "eyes":
+            self.switchBodytoEyes()
         x0, y0, x1, y1 = get_circle_coords(400, 200, 30, self.canvas)
         self.canvas.tag_raise(self.pupil1)
         self.canvas.coords(self.pupil1, x0, y0, x1, y1)
         x0, y0, x1, y1 = get_circle_coords(550, 200, 30, self.canvas)
         self.canvas.coords(self.pupil2, x0, y0, x1, y1)
 
-    def removeCurrentCanvas(self):
-        self.canvas.destroy()
-
-class RobotBody:
-    def __init__(self, window) -> None:
-        self.window = window
-        # Create the canvas that sits ontop the window
-        self.canvas = tk.Canvas(self.window, bg='gray', height=480, width=800)
-        # Create the initial body
-        self.leftLeg = self.canvas.create_line(325,425,400,300, width=4) # Left leg
-        self.rightLeg = self.canvas.create_line(475,425,400,300, width=4) # Right leg
-        self.back = self.canvas.create_line(400,300,400,150, width=4) # back
-        # self.pupil2 = create_circle(500, 200, 30, self.canvas, 3, "black")  # Pupil 2
-        self.head = create_circle(400, 100, 50, self.canvas, 3, "gray")
-        self.leftEye = create_circle(385, 85, 10, self.canvas, 3, "black")
-        self.rightEye = create_circle(415, 85, 10, self.canvas, 3, "black")
-        self.leftArm = self.canvas.create_line(325,250,400,175, width=4) # Left arm
-        self.rightArm = self.canvas.create_line(475,250,400,175, width=4) # Right arm
-        self.canvas.pack()
-
-    def idle(self):
+    def bodyIdle(self, key):
+        print("Key:", key)
         x0, y0, x1, y1 = get_circle_coords(385, 85, 10, self.canvas)
         self.canvas.coords(self.leftEye, x0, y0, x1, y1)
         x0, y0, x1, y1 = get_circle_coords(415, 85, 10, self.canvas)
@@ -103,15 +124,16 @@ class RobotBody:
         self.canvas.coords(self.leftArm, 345,275,400,175)
         self.canvas.coords(self.rightArm, 445,265,400,175)
 
-    def walkLeft(self, count = 1000):
-        while True: # Implement a trigger to break or use some event trigger
-            if count > 3000:
-                break
-            self.window.after(count, self.halfStepLeft)
-            count += 1000
-            self.window.after(count, self.fullStepLeft)
-            count += 1000
-        return count
+    def walkLeft(self, key):
+        #  First remove the other canvas and create a new canvas
+        if self.curr_animation != "body":
+            self.removeCurrentCanvas()
+            self.createBody()
+        self.count = 500
+        self.window.after(self.count, self.halfStepLeft)
+        self.count += 500
+        self.window.after(self.count, self.fullStepLeft)
+        self.count += 500
 
     def halfStepRight(self):
         self.canvas.coords(self.leftEye, 0, 0, 0, 0)
@@ -128,67 +150,40 @@ class RobotBody:
         self.canvas.coords(self.leftArm, 350,275,400,175)
         self.canvas.coords(self.rightArm, 485,235,400,175)
 
-    def walkRight(self, count = 1000):
-        start_cnt = count
-        
-        while True: # Implement a trigger to break or use some event trigger
-            if count > start_cnt+3000:
-                break
-            self.window.after(count, self.halfStepRight)
-            count += 1000
-            self.window.after(count, self.fullStepRight)
-            count += 1000
-        return count    
+    def walkRight(self, key):
+        if self.curr_animation != "body":
+            self.removeCurrentCanvas()
+            self.createBody()
+        self.count = 500
+        self.window.after(self.count, self.halfStepRight)
+        self.count += 500
+        self.window.after(self.count, self.fullStepRight)
+        self.count += 500
 
     def removeCurrentCanvas(self):
+        self.canvas.delete("all")
         self.canvas.destroy()
 
+
+### NOTE: When switching between the body and eyes dont switch to quickly or the eyes will break and look weird
 def main():
     window = tk.Tk()
     window.title("Robot 35")
     window.geometry('800x480') # Sets the width x height of the window
     
-    face = RobotFace(window)
-
+    # face = RobotFace(window)
+    robot_animations = RobotAnimations(window)
     #TODO: Key Bindings will need to match what the robot uses
     # i.e.) lookUp will use the same key used in keyboard_control.py to make the robot loop up
-    window.bind("w", face.lookUp)
-    window.bind("a", face.lookleft)
-    window.bind("s", face.lookDown)
-    window.bind("d", face.lookRight)
-
-    # window.bind("left", RobotBody.walkLeft)
+    window.bind("w", robot_animations.lookUp)
+    window.bind("a", robot_animations.lookleft)
+    window.bind("s", robot_animations.lookDown)
+    window.bind("d", robot_animations.lookRight)
+    window.bind("<Left>", robot_animations.walkLeft)
+    window.bind("<Right>", robot_animations.walkRight)
+    window.bind("<space>", robot_animations.bodyIdle)
 
     window.mainloop()
-
-
-def test ():
-    window = tk.Tk()
-    window.title("Robot 35")
-    window.geometry('800x480') # Sets the width x height of the window
-    
-
-
-    ###  Test the body animations
-    body = RobotBody(window)
-    # count = body.walkLeft()
-    # window.after(count, body.idle) 
-    # count += 1000
-    # count = body.walkRight(count)
-    # count+=1000
-
-    ###  Test for the face animations using time based queuing
-    # window.after(count, body.removeCurrentCanvas)
-    face = RobotFace(window)
-    # count+=1000
-    # window.after(count, face.lookleft)
-    # window.after(2000, face.lookRight)
-    # window.after(3000, face.lookDown)
-    # window.after(4000, face.lookUp)
-    # window.after(5000, face.idleEyes)
-    
-    window.mainloop()
-    
 
 if __name__ == "__main__":
     main()
