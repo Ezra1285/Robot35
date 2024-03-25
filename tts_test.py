f7c2794eb0c70e016188fe9401006b775b314fd3
ryang <ryangrossmn@gmail.com>
2024-03-25 13:58:43 -0600

    added tts test file

--- tts_test.py
+++ tts_test.py
@@ -1,27 +1,30 @@
 import pyttsx3
 
 
-global speech
-class Text_To_Speech:
-    global speech
-    def __init__(self):
-        self.engine = pyttsx3.init()
-        self.engine.setProperty('rate', 150) # speech rate
 
-    def speak(self):
+global speech # speech is a global so it can be set anywhere we want the robot to say something
+# class Text_To_Speech:
+    # global speech 
+    # def __init__(self):
+    #     self.engine = pyttsx3.init()
+    #     self.engine.setProperty('rate', 150) # speech rate
+
+# def speak(self):
+def speak():
         global speech
+        engine = pyttsx3.init() 
         if (speech != ''):
             while(True):    
-                self.engine.say(speech)
-                self.engine.runAndWait()
+                engine.say(speech)
+                engine.runAndWait()
                 speech = ''
 
 def main():
     global speech
     speech = "Hello World"
-    tts = Text_To_Speech
-    tts.__init__(tts)
-    tts.speak(tts)
+    # tts = Text_To_Speech
+    # tts.speak(tts)
+    speak()
 
 if __name__ == '__main__':
     main()
