bdb17fcfa837b6cff3764922704ac90d77ec6d1f
Ezra1285 <ezraskoog@yahoo.com>
2024-03-27 12:16:28 -0600

    t15

--- thread_stuff.py
+++ thread_stuff.py
@@ -87,7 +87,7 @@ class RobotAnimations:
         self.canvas.coords(self.pupil1, x0, y0, x1, y1)
         x0, y0, x1, y1 = get_circle_coords(500, 200, 30, self.canvas)
         self.canvas.coords(self.pupil2, x0, y0, x1, y1)
-        self.canvas.coords(self.mouth)
+        self.canvas.coords(self.mouth, 350, 300, 500, 350)
 
     def lookUp(self, button_event):
         if self.curr_animation != "eyes":
