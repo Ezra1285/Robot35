bb967c5e6780404de7f3939a3be425f2fae57e87
Ezra1285 <ezraskoog@yahoo.com>
2024-04-09 20:11:56 -0600

    proj 6 test 2

--- project_6.py
+++ project_6.py
@@ -3,17 +3,15 @@ import pyttsx3
 import os
 from openai import OpenAI
 
-client = OpenAI()
-os.environ["OPENAI_API_KEY"] = "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"
 
 # Define a function to interact with ChatGPT
-def chat_with_gpt(prompt, voice='surprise_me'):
+def chat_with_gpt(prompt, client, voice='surprise_me'):
     
     # Send the prompt to ChatGPT and get the response
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": f"{question} respond like a {voice} and give a short answer."}
+        {"role": "system", "content": f"{prompt} respond like a {voice} and give a short answer."}
     ],
     max_tokens=10
     )
@@ -29,8 +27,10 @@ def speak(speech):
 
 # Example usage:
 if __name__ == "__main__":
+    client = OpenAI()
+    os.environ["OPENAI_API_KEY"] = "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"
     question = "What is the meaning of life?"
     chosen_voice = 'pirate'  # Change the voice as desired
-    response = chat_with_gpt(question, voice=chosen_voice)
+    response = chat_with_gpt(question, client, voice=chosen_voice)
     print(response)
     speak(response)
