0118bf9d12045e5a415b9df409eebb7971bd783d
Ezra1285 <ezraskoog@yahoo.com>
2024-04-10 12:52:08 -0600

    proj 6 test 29

--- project_6.py
+++ project_6.py
@@ -15,8 +15,8 @@ def chat_with_gpt(prompt, client, voice='surprise_me'):
     )
     # content = response.choices[0].message["content"]
     content = response.choices
-    print("Choice: ", content)
-    content = content[0].message
+    # print("Choice: ", content)
+    content = content[0].message.content
     print("Choice: ", content)
     # content = content.messages
     return content
