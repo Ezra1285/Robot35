452eae7c0fbc26d3da63e5ecb1e8400a52918fde
Ezra1285 <ezraskoog@yahoo.com>
2024-04-18 14:16:14 -0600

    t

---
+++ project_8.py
@@ -0,0 +1,34 @@
+import serial
+import tkinter as tk
+from maestro import Controller 
+import time
+import pyttsx3
+
+# try: #print("in try") 
+#     ser = serial.Serial() 
+#     ser.port = '/dev/ttyUSB0' 
+#     ser.baudrate = 115200 
+#     ser.bytesize = serial.EIGHTBITS 
+#     ser.parity = serial.PARITY_NONE 
+#     ser.stopbits = serial.STOPBITS_ONE 
+#     ser.timeout = 1 ser.open()
+#     Ser.readline()
+
+class LocationChip:
+
+    def __init__(self):
+        ser = serial.Serial() 
+        ser.port = '/dev/ttyUSB0' 
+        ser.baudrate = 115200 
+        ser.bytesize = serial.EIGHTBITS 
+        ser.parity = serial.PARITY_NONE 
+        ser.stopbits = serial.STOPBITS_ONE 
+        ser.timeout = ser.open() 
+        # First read line is HEX and next one is decimal
+        text = ser.readline()
+        print("Text found: ", text)
+
+
+if __name__ == "__main__":
+    chip = LocationChip()
+    print("Done")
