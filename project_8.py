1b1eda43dfa049c4e3aaf7662ca6c360f1abd567
ryang <ryangrossmn@gmail.com>
2024-04-18 14:16:30 -0600

    proj 8 start

--- project_8.py
+++ project_8.py
@@ -14,12 +14,17 @@ import pyttsx3
 #     ser.timeout = 1 ser.open()
 #     Ser.readline()
 
-def __init__(self):
-    ser = serial.Serial() 
-    ser.port = '/dev/ttyUSB0' 
-    ser.baudrate = 115200 
-    ser.bytesize = serial.EIGHTBITS 
-    ser.parity = serial.PARITY_NONE 
-    ser.stopbits = serial.STOPBITS_ONE 
-    ser.timeout = 1 ser.open() 
-    Ser.readline()
+
+class Range:
+    def __init__(self):
+        ser = serial.Serial() 
+        ser.port = '/dev/ttyUSB0' 
+        ser.baudrate = 115200 
+        ser.bytesize = serial.EIGHTBITS 
+        ser.parity = serial.PARITY_NONE 
+        ser.stopbits = serial.STOPBITS_ONE 
+        ser.timeout = 1 ser.open() 
+        Ser.readline()
+
+    def getDistance():
+    
