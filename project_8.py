dbbe0fbdf2f7548860451a70812aac3f63cf8402
Ezra1285 <ezraskoog@yahoo.com>
2024-04-19 12:32:59 -0600

    test num 30

--- project_8.py
+++ project_8.py
@@ -100,8 +100,8 @@ class LocationChip:
     def exitBox(self):
         self.robot_contol.turnRight(800)
         time.sleep(3)
-        self.robot_contol.moveFoward(800)
-        time.sleep(2)
+        self.robot_contol.moveFoward(1200)
+        time.sleep(3)
         self.robot_contol.defualtMotors()
         speak("I have exited the box")    
 
