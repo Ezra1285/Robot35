9a2fe02deaf5caa8d9b5512241ac3d54dbe2fe7a
Ezra1285 <ezraskoog@yahoo.com>
2024-04-19 12:18:37 -0600

    test num 24

--- project_8.py
+++ project_8.py
@@ -84,6 +84,7 @@ class LocationChip:
         data = self.readData()
         print("Type", type(data[1]))
         print("Val", data[0])
+        data[3] = 0
         self.cords = {'a0':float(data[1]), 'a1':float(data[2]), 'a2':float(data[3]), 'a3':float(data[4])} #messing with indexing here
         self.cords['a3'] = 1000
         closest_cord = min(self.cords, key=self.cords.get)
