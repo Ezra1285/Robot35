5170c483c45f9b2ebb53d82da2213012bccd1ec2
Ezra1285 <ezraskoog@yahoo.com>
2024-04-19 12:04:24 -0600

    test num 17

--- stuff_nad_things.py
+++ stuff_nad_things.py
@@ -35,13 +35,23 @@ class myTest:
 
     def spinCycle(self):
         self.robot_contol.lookUp(1800)
-        time.sleep(3)
+        time.sleep(2)
         self.cords = self.readData()  # read initial spot while looking up
         print("Initial Up cords:", self.cords)
         self.robot_contol.turnRight(900)
         time.sleep(4)
         self.cords = self.readData()  # read initial spot while looking up
         print("Turn 1 cords:", self.cords)
+
+        self.robot_contol.turnRight(900)
+        time.sleep(4)
+        self.cords = self.readData()  # read initial spot while looking up
+        print("Turn 2 cords:", self.cords)
+
+        self.robot_contol.turnRight(900)
+        time.sleep(4)
+        self.cords = self.readData()  # read initial spot while looking up
+        print("Turn 3 cords:", self.cords)
         # self.robot_contol.defualtMotors()
         # self.cords = self.readData()
 
