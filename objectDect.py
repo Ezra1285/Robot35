9e23c8301acf2dd03e7a6e4ba4ce9db88a255bd1
Ezra1285 <ezraskoog@yahoo.com>
2024-04-24 12:00:17 -0600

    t 9 test 7

---
+++ objectDect.py
@@ -0,0 +1,43 @@
+import RPi.GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG_PIN = 23
+ECHO_PIN = 24
+
+GPIO.setup(TRIG_PIN, GPIO.OUT)
+GPIO.setup(ECHO_PIN, GPIO.IN)
+
+def get_distance():
+    GPIO.output(TRIG_PIN, False)
+    time.sleep(0.1)
+
+    GPIO.output(TRIG_PIN, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG_PIN, False)
+
+    timeout = time.time()
+    while GPIO.output(ECHO_PIN) == 0:
+        if (time.time() - timeout) > 3:
+            print('timeout occured')
+            return None
+
+    pulse_start = time.time()
+    timeout = time.time()
+    while GPIO.input(ECHO_PIN) == 1:
+        if (time.time() - timeout) > 3:
+            print('timeout occured')
+            return None
+    print("AAA")
+    pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
+    return distance
+
+while True:
+    get_distance()
