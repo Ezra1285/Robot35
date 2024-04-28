5741e223e31ad17107c7676ae974d03ddbfca971
Ezra1285 <ezraskoog@yahoo.com>
2024-04-27 18:16:32 -0600

    saturday work 29

--- project_8.py
+++ project_8.py
@@ -288,7 +288,7 @@ class RobotControl():
             self.motors = 1510
         if(self.motors > 7900):
             self.motors = 7900
-        # self.tango.setSpeed(MOTORS, 10)
+        self.tango.setSpeed(MOTORS, 50)
         self.tango.setAccel(MOTORS, 200)
         self.tango.setTarget(MOTORS, self.motors)
 
