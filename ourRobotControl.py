87be29ca244044fde23be4a876b56d8cdaca76f6
RohanKamat14 <rohannkamat@gmail.com>
2024-04-30 12:08:13 -0600

    test

--- ourRobotControl.py
+++ ourRobotControl.py
@@ -22,21 +22,21 @@ class RobotControl():
         if(self.headTilt < 1510):
             self.headTilt = 1510
         self.tango.setTarget(HEADTILT, self.headTilt)
-        print("Look down")
+        #print("Look down")
 
     def lookUp(self, amount=200):
         self.headTilt += amount
         if(self.headTilt > 7900):
             self.headTilt = 7900
         self.tango.setTarget(HEADTILT, self.headTilt)
-        print("Look up")
+        #print("Look up")
 
     def lookRight(self, amount=200):
         self.headTurn += amount
         if(self.headTurn > 7900):
             self.headTurn = 7900
         self.tango.setTarget(HEADTURN, self.headTurn)
-        print("Look Right")
+        #print("Look Right")
 
     def defaultHead(self):
         self.headTilt = 6000
@@ -49,21 +49,21 @@ class RobotControl():
         if(self.headTurn < 1510):
             self.headTurn = 1510
         self.tango.setTarget(HEADTURN, self.headTurn)
-        print("Look left")
+        #print("Look left")
         
     def waistLeft(self, amount=200):
         self.body -= amount
         if(self.body < 1510):
             self.body = 1510
         self.tango.setTarget(BODY, self.body)
-        print ('waist left')
+        #print ('waist left')
 
     def waistRight(self, amount=200):    
         self.body += amount
         if(self.body > 7900):
             self.body = 7900
         self.tango.setTarget(BODY, self.body)
-        print("waist right")
+        #print("waist right")
         
             
     def moveFoward(self, amount=200):
@@ -71,7 +71,7 @@ class RobotControl():
         if(self.motors > 7900):
             self.motors = 7900
         self.tango.setTarget(MOTORS, self.motors)
-        print("Foward")
+        #print("Foward")
     
     def moveBackwards(self, amount=200):
         self.motors -= amount
@@ -84,7 +84,7 @@ class RobotControl():
         if(self.turn > 7400):
             self.turn = 7400
         self.tango.setTarget(TURN, self.turn)
-        print("Turning Right")
+        #print("Turning Right")
 
     def slowTurnRight(self, amount=200):
         self.tango.setSpeed(TURN, 3)
@@ -93,21 +93,21 @@ class RobotControl():
         if(self.turn > 7400):
             self.turn = 7400
         self.tango.setTarget(TURN, self.turn)
-        print("Turning Right")
+        #print("Turning Right")
 
     def turnLeft(self, amount=200):
         self.turn -= amount
         if(self.turn <2110):
             self.turn = 2110
         self.tango.setTarget(TURN, self.turn)
-        print("Turning left")
+        #print("Turning left")
 
     def defualtMotors(self):
         self.motors = 6000
         self.turn = 6000
         self.tango.setTarget(MOTORS, self.motors)
         self.tango.setTarget(TURN, self.turn)
-        print("Defaulting motors")
+        #print("Defaulting motors")
 
     def defualtEverything(self):
         self.motors = 6000
