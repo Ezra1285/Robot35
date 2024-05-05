4da7bd38b21f4a2c049ed12a5ed6dc662df465c8
RohanKamat14 <rohannkamat@gmail.com>
2024-05-05 14:57:09 -0600

    test

--- final.py
+++ final.py
@@ -39,7 +39,7 @@ def findSquare(quadrant, location):
     robot_cotrol = RobotControl()
     while True:
         robot_cotrol.turnRight(900)
-        time.sleep(1.5)
+        time.sleep(2.5)
         robot_cotrol.defualtMotors()
         t2 = threading.Thread(target=robot_cotrol.moveBackwards(1000))
         t2.start()
