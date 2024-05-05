73fdc4794c2f7cc75a9188c246c6517d32eb7815
RohanKamat14 <rohannkamat@gmail.com>
2024-05-05 14:52:44 -0600

    tets

--- final.py
+++ final.py
@@ -40,6 +40,7 @@ def findSquare(quadrant, location):
     while True:
         robot_cotrol.turnLeft(900)
         time.sleep(1)
+        robot_cotrol.defualtMotors()
         t2 = threading.Thread(target=robot_cotrol.moveBackwards(1000))
         t2.start()
         t2.join()
