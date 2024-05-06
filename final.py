606f996646fbeb8b20d48832e57ff085a5055fa7
RohanKamat14 <rohannkamat@gmail.com>
2024-05-06 04:10:47 -0600

    t

--- final.py
+++ final.py
@@ -90,18 +90,12 @@ def findQuadrant(location, quadrant):
     robot_cotrol.defualtMotors()
     data2 = location.readData()
     cords_dict2 = {'a0':float(data2[0]), 'a1':float(data2[1]), 'a2':float(data2[2]), 'a3':float(data2[3])} #messing with indexing here
-    if cords_dict.get('a3') > cords_dict2.get('a3') and cords_dict.get('a2') > cords_dict2.get('a2'):
-        print("GWAAAAAAAAAAAAAAAAAAAAA")
-        return
     if cords_dict.get(quadrant) < cords_dict2.get(quadrant):
-        robot_cotrol.turnLeft(950)
-        time.sleep(3)
+        robot_cotrol.moveFoward(700)
+        time.sleep(2)
         robot_cotrol.defualtEverything()
         return
     else:
-        robot_cotrol.turnRight(900)
-        time.sleep(1)
-        robot_cotrol.defualtMotors()
         robot_cotrol.moveBackwards(800)
         time.sleep(2.5)
 
